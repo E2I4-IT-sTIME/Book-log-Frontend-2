97555730fd67dde65bc08d9bbcbe13f7cc7929ac
EUNA <beni1026@naver.com>
2022-11-11 17:49:39 +0900

    feat: 포트폴리오 카드& 목록

--- booklog/components/portfolio/PortfolioCard.tsx
+++ booklog/components/portfolio/PortfolioCard.tsx
@@ -2,39 +2,63 @@ import Image from "next/image";
 import noSign from "../../res/noSign.svg";
 
 const PortfolioCard = (props: any) => {
-  const { title, sub } = props;
+  const { title, sub, backgroundImg, thumnailArr } = props;
+  const brText = (text: string) => {
+    return (
+      <span>
+        {text.split("\n").map((txt: String) => (
+          <>
+            {txt}
+            <br />
+          </>
+        ))}
+      </span>
+    );
+  };
   return (
     <>
-      <div className="container">
+      <div className="portCard-container">
         <div className="text-box">
-          <div className="title">{title}</div>
-          <div className="sub">{sub}</div>
+          <div className="title">{brText(title)}</div>
+          <div className="sub">{brText(sub)}</div>
         </div>
         <div className="img-box">
           <div className="profile-box">
-            <Image src={noSign} width="20px" height="20px" />
+            <Image src={noSign} width="50px" height="50px" />
           </div>
           <div className="thumnail-box">
-            <div className="thumnail"></div>
-            <div className="thumnail"></div>
-            <div className="thumnail"></div>
+            {thumnailArr.map((imgSrc: string) => (
+              <Image
+                src={imgSrc}
+                width={30}
+                height={70}
+                style={{
+                  borderRadius: "10px",
+                }}
+              />
+            ))}
           </div>
         </div>
       </div>
       <style jsx>{`
-        .container {
-          background: url("../../res/protBackground.png");
+        .portCard-container {
+          box-shadow: 0px 4px 15px rgba(0, 0, 0, 0.15);
+          background: url(${backgroundImg}) no-repeat center center;
+          background-size: cover;
           display: flex;
-          flex-direction: column;
-          width: 45%;
-          height: 250px;
+          flex-direction: row;
+          width: 48%;
+          height: 260px;
           padding: 10px;
+          border-radius: 20px;
+          padding: 30px;
+          cursor: pointer;
         }
         .text-box {
           width: 60%;
           height: 100%;
           display: flex;
-          flex-direction: row;
+          flex-direction: column;
           justify-content: flex-end;
           color: white;
           gap: 10px;
@@ -49,11 +73,16 @@ const PortfolioCard = (props: any) => {
         .img-box {
           width: 40%;
           display: flex;
-          flex-direction: row;
+          flex-direction: column;
           justify-content: space-between;
+        }
+        .profile-box {
+          display: flex;
+          justify-content: flex-end;
         }
         .thumnail-box {
           display: flex;
+          justify-content: flex-end;
         }
       `}</style>
     </>
--- booklog/pages/portfolio.tsx
+++ booklog/pages/portfolio.tsx
@@ -5,11 +5,36 @@ import PortfolioNav from "../components/portfolio/PortfolioNav";
 import { useRecoilState } from "recoil";
 import { CurrentLayout, ClubLayoutState } from "../states/recoilLayoutState";
 import { useEffect } from "react";
+import PortfolioCard from "../components/portfolio/PortfolioCard";
 
 const DUMMY = [
   {
-    title: "",
-    sub: "",
+    id: 1,
+    title: "내가 1년 동안 자존감을\n 높였던 방법",
+    sub: "자존감 밑바닥을 찍던 내가,\n 자존감을 높일 수 있었던 1년의 기록.",
+    backgroundImg: "/portBackground.png",
+    thumnailArr: ["/tmp.jpg", "/tmp.jpg", "/tmp.jpg"],
+  },
+  {
+    id: 2,
+    title: "내가 1년 동안 자존감을\n 높였던 방법",
+    sub: "자존감 밑바닥을 찍던 내가,\n 자존감을 높일 수 있었던 1년의 기록.",
+    backgroundImg: "/portBackground.png",
+    thumnailArr: ["/tmp.jpg", "/tmp.jpg", "/tmp.jpg"],
+  },
+  {
+    id: 3,
+    title: "내가 1년 동안 자존감을\n 높였던 방법",
+    sub: "자존감 밑바닥을 찍던 내가,\n 자존감을 높일 수 있었던 1년의 기록.",
+    backgroundImg: "/portBackground.png",
+    thumnailArr: ["/tmp.jpg", "/tmp.jpg", "/tmp.jpg"],
+  },
+  {
+    id: 4,
+    title: "내가 1년 동안 자존감을\n 높였던 방법",
+    sub: "자존감 밑바닥을 찍던 내가,\n 자존감을 높일 수 있었던 1년의 기록.",
+    backgroundImg: "/portBackground.png",
+    thumnailArr: ["/tmp.jpg", "/tmp.jpg", "/tmp.jpg"],
   },
 ];
 
@@ -29,7 +54,19 @@ const portfolio: NextPage = () => {
       <div className="container">
         <PortfolioNav />
         <PageTitle title={title} sub={sub} />
-        <div className="portfolio-list"></div>
+        <div className="portfolio-list">
+          {DUMMY.map((card) => {
+            return (
+              <PortfolioCard
+                key={card.id}
+                title={card.title}
+                sub={card.sub}
+                backgroundImg={card.backgroundImg}
+                thumnailArr={card.thumnailArr}
+              />
+            );
+          })}
+        </div>
       </div>
       <style jsx>{`
         .container {
@@ -39,17 +76,10 @@ const portfolio: NextPage = () => {
           padding-bottom: 20%;
           background: linear-gradient(#faf5e4 35%, #fff 10%);
         }
-      `}</style>
-      <style jsx>{`
-        .dd {
-          margin-top: 500px;
-        }
-        .container {
-          padding-left: 30%;
-          padding-right: 5%;
-          padding-top: 10%;
-          padding-bottom: 20%;
-          background: linear-gradient(#faf5e4 35%, #fff 10%);
+        .portfolio-list {
+          display: flex;
+          flex-wrap: wrap;
+          gap: 20px;
         }
       `}</style>
     </>
