ebebf440096117c50529e98f77486ffa08b47010
EUNA <beni1026@naver.com>
2022-11-16 03:53:36 +0900

    feat : 공통 api 함수 작성

---
+++ booklog/components/api.tsx
@@ -0,0 +1,19 @@
+import axios from "axios";
+
+export const API_END_POINT = "https://localhost:8080";
+
+export const request = async (url, options = {}) => {
+  try {
+    const res = await axios(`${API_END_POINT}${url}`, {
+      ...options,
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    if (res.status === 200) return await res.data();
+
+    throw new Error("API 호출 오류!");
+  } catch (e) {
+    alert(e);
+  }
+};
