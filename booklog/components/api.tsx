11079acc4ed41ebe5068ee6b1abce9eceaa77968
EUNA <beni1026@naver.com>
2022-12-01 02:31:17 +0900

    feat: 개별 포트폴리오 조회 통신

--- booklog/components/api.tsx
+++ booklog/components/api.tsx
@@ -7,6 +7,22 @@ const API = axios.create({
   },
 });
 
+// 서평 추가
+export const addReviewtoPort = async (postData, userIndex) => {
+  try {
+    const res = await API.post(`/auth/user/${userIndex}/review`, postData, {
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem("access_token")}`,
+      },
+    });
+    if (res.status === 200) {
+      return true;
+    }
+  } catch (err) {
+    console.error(err);
+  }
+};
+
 // 서평 생성
 export const postReveiwData = async (postData, userIndex) => {
   try {
@@ -55,10 +71,26 @@ export const fetchPortfolioList = async (userIndex) => {
   }
 };
 
-// 포트폴리오 생성
-export const postPortfolioData = async (postData, userIndex) => {
+// 포트폴리오 개별 조회
+export const fetchPortfolio = async (userIndex, port_id) => {
   try {
-    const res = await API.post(`/auth/user/${userIndex}/portfolio`, postData, {
+    const res = await API.get(`/auth/user/${userIndex}/portfolios/${port_id}`, {
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem("access_token")}`,
+      },
+    });
+    if (res.status === 200) {
+      return res.data;
+    }
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+// 포트폴리오 생성
+export const postPortfolioData = async (formData, userIndex) => {
+  try {
+    const res = await API.post(`/auth/user/${userIndex}/portfolio`, formData, {
       headers: {
         Authorization: `Bearer ${localStorage.getItem("access_token")}`,
       },
